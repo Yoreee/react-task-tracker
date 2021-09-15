3c7ad43011b1c71b872af05d5de55b1b9906a45f
Yoreee <rerebacchus@gmail.com>
2021-09-15 02:09:12 +0000

    new changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import './App.css';
 // import React from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState}  from 'react';
+import Task from './components/Task';
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "one",
+        day: "M",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "two",
+        day: "Tu",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "three",
+        day: "W",
+        reminder: true
+    },
+    {
+        id: 4,
+        text: "four",
+        day: "Th",
+        reminder: true
+    },
+    {
+        id: 5,
+        text: "five",
+        day: "F",
+        reminder: true
+    }
+])
+
+const deleteTask = (id) => {
+  console.log('DELETE', id);
+  setTasks(tasks.filter((task) => task.id !== id));
+}
+
   return (
     <div className="container">
       <Header></Header>
+      <Tasks tasks={tasks} onDelete={deleteTask}></Tasks>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,8 @@ const Button = ({color, text, onClick}) => {
     return (
             <button 
             onClick={onClick}
-            className='btn' style={{backgroundColor: color}}>{text}</button>
+            className='btn' style={{backgroundColor: color}}>{text}
+            </button>
     )
 }
 
