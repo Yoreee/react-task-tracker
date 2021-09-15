6cda28e11aec38723b07f619aaf5134a2e79252c
Yoreee <rerebacchus@gmail.com>
2021-09-15 02:16:55 +0000

    remove comments

--- src/App.js
+++ src/App.js
@@ -53,10 +53,4 @@ const deleteTask = (id) => {
   );
 }
 
-
-// class App extends React.Component {
-//   render() {
-//     return <h1>Hello from Class Component</h1>
-//   }
-// }
 export default App;
